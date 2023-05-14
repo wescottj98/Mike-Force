15bac47b433d08d9851acdb2922525eb5d6acf3c
dijksterhuis <general@dijksterhuis.co.uk>
2023-05-14 18:12:26 +0100

    [UPDATE] Add a configurable zone size for each map (very useful for the bra map!)

--- maps/vn_khe_sanh/map_config/init.hpp
+++ maps/vn_khe_sanh/map_config/init.hpp
@@ -8,6 +8,7 @@ class map_config {
 	max_water_supply_per_zone = 2;
 	max_vehicle_depots_per_zone = 3;
 	starting_zones[] = {"zone_khe_sanh", "zone_kok", "zone_french_fort"};
+	bn_zone_radius = 1000;
 	class zones {
 		#include "zones.hpp"
 	};
